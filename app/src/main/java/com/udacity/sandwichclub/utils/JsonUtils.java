df497845ec2de881ad822b3673f9023de2cf502a
Felipe Joglar <felipejoglar@gmail.com>
2018-02-20 17:14:04 +0100

    JsonUtils improvements

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -11,20 +11,28 @@ import java.util.List;
 
 public class JsonUtils {
 
+    private static final String JSON_NAME_KEY = "name";
+    private static final String JSON_MAIN_NAME_KEY = "mainName";
+    private static final String JSON_ALSO_KNOWN_KEY = "alsoKnownAs";
+    private static final String JSON_PLACE_OF_ORIGIN_KEY = "placeOfOrigin";
+    private static final String JSON_DESCRIPTION_KEY = "description";
+    private static final String JSON_IMAGE_KEY = "image";
+    private static final String JSON_INGREDIENTS_KEY = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
 
         Sandwich sandwich = new Sandwich();
 
         try {
             JSONObject jsonSandwich = new JSONObject(json);
-            JSONObject name = jsonSandwich.getJSONObject("name");
+            JSONObject name = jsonSandwich.getJSONObject(JSON_NAME_KEY);
 
-            sandwich.setMainName(name.getString("mainName"));
-            sandwich.setAlsoKnownAs(getListFromJsonArray(name.getJSONArray("alsoKnownAs")));
-            sandwich.setPlaceOfOrigin(jsonSandwich.getString("placeOfOrigin"));
-            sandwich.setDescription(jsonSandwich.getString("description"));
-            sandwich.setImage(jsonSandwich.getString("image"));
-            sandwich.setIngredients(getListFromJsonArray(jsonSandwich.getJSONArray("ingredients")));
+            sandwich.setMainName(name.optString(JSON_MAIN_NAME_KEY));
+            sandwich.setAlsoKnownAs(getListFromJsonArray(name.getJSONArray(JSON_ALSO_KNOWN_KEY)));
+            sandwich.setPlaceOfOrigin(jsonSandwich.optString(JSON_PLACE_OF_ORIGIN_KEY));
+            sandwich.setDescription(jsonSandwich.optString(JSON_DESCRIPTION_KEY));
+            sandwich.setImage(jsonSandwich.optString(JSON_IMAGE_KEY));
+            sandwich.setIngredients(getListFromJsonArray(jsonSandwich.getJSONArray(JSON_INGREDIENTS_KEY)));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -37,7 +45,7 @@ public class JsonUtils {
         List<String> result = new ArrayList<>();
 
         for (int i = 0; i < jsonArray.length(); i++) {
-            result.add(jsonArray.getString(i));
+            result.add(jsonArray.optString(i));
         }
 
         return result;
